2728a0aff1a7adc465a5352e66dec80be7267b80
MuhammadAliGabol <aligabol2002@gmail.com>
2020-09-28 12:48:29 +0500

    Added Axios

---
+++ app/constants.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//API URL
+export const API_URL = 'https://btk.financebakerz.com/api/v1';
+
+//API End Points
+export const SIGNUP = `${API_URL}/auth/signup`;
--- app/screens/SignUp/index.js
+++ app/screens/SignUp/index.js
@@ -3,7 +3,9 @@ import {View, KeyboardAvoidingView, Platform} from 'react-native';
 import {BaseStyle, useTheme} from '@config';
 import {Header, SafeAreaView, Icon, Button, TextInput} from '@components';
 import styles from './styles';
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import axios from 'axios';
+import {SIGNUP} from '../../constants';
 
 export default function SignUp({navigation}) {
   const {colors} = useTheme();
@@ -26,7 +28,8 @@ export default function SignUp({navigation}) {
    *
    */
   const onSignUp = () => {
-    if (name == '' || email == '' || phone == '' || password == '') {
+    if (name === '' || email === '' || phone === '' || password == '') {
+
       setSuccess({
         ...success,
         name: name != '' ? true : false,
@@ -41,6 +44,22 @@ export default function SignUp({navigation}) {
         navigation.navigate('SignIn');
       }, 500);
     }
+
+    console.log('SIGNUP: ', SIGNUP);
+
+    axios.post(SIGNUP, {
+      name,
+      email,
+      phone,
+      password
+    })
+        .then(response => {
+          console.log(response)
+        })
+        .catch(error => {
+          console.log(error.response)
+        });
+
     console.log('Username: ', name);
     console.log('Email: ', email);
     console.log('Phone: ', phone);
@@ -52,70 +71,70 @@ export default function SignUp({navigation}) {
     android: 20,
   });
   return (
-    <SafeAreaView style={BaseStyle.safeAreaView} forceInset={{top: 'always'}}>
-      <Header
-        title={t('sign_up')}
-        renderLeft={() => {
-          return (
-            <Icon
-              name="arrow-left"
-              size={20}
-              color={colors.primary}
-              enableRTL={true}
+      <SafeAreaView style={BaseStyle.safeAreaView} forceInset={{top: 'always'}}>
+        <Header
+            title={t('sign_up')}
+            renderLeft={() => {
+              return (
+                  <Icon
+                      name="arrow-left"
+                      size={20}
+                      color={colors.primary}
+                      enableRTL={true}
+                  />
+              );
+            }}
+            onPressLeft={() => {
+              navigation.goBack();
+            }}
+        />
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'android' ? 'height' : 'padding'}
+            keyboardVerticalOffset={offsetKeyboard}
+            style={{flex: 1}}>
+          <View style={styles.contain}>
+            <TextInput
+                onChangeText={text => setName(text)}
+                placeholder= "Username"
+                success={success.name}
+                value={name}
             />
-          );
-        }}
-        onPressLeft={() => {
-          navigation.goBack();
-        }}
-      />
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'android' ? 'height' : 'padding'}
-        keyboardVerticalOffset={offsetKeyboard}
-        style={{flex: 1}}>
-        <View style={styles.contain}>
-          <TextInput
-            onChangeText={text => setName(text)}
-            placeholder= "Username"
-            success={success.name}
-            value={name}
-          />
-          <TextInput
-            style={{marginTop: 10}}
-            onChangeText={text => setEmail(text)}
-            placeholder= "Email"
-            keyboardType="email-address"
-            success={success.email}
-            value={email}
-            autoCapitalize="none"
-          />
-          <TextInput
-            style={{marginTop: 10}}
-            onChangeText={text => setPhone(text)}
-            placeholder="Phone"
-            keyboardType="numeric"
-            success={success.phone}
-            value={phone}
-            autoCapitalize="none"
-          />
-          <TextInput
-            style={{marginTop: 10}}
-            onChangeText={text => setPassword(text)}
-            placeholder="Password"
-            secureTextEntry={true}
-            success={success.password}
-            value={password}
-            autoCapitalize="none"
-          />
-          <Button
-            full
-            style={{marginTop: 20}}
-            loading={loading}
-            onPress={() => onSignUp()}>
-            {t('sign_up')}
-          </Button>
-        </View>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+            <TextInput
+                style={{marginTop: 10}}
+                onChangeText={text => setEmail(text)}
+                placeholder= "Email"
+                keyboardType="email-address"
+                success={success.email}
+                value={email}
+                autoCapitalize="none"
+            />
+            <TextInput
+                style={{marginTop: 10}}
+                onChangeText={text => setPhone(text)}
+                placeholder="Phone"
+                keyboardType="numeric"
+                success={success.phone}
+                value={phone}
+                autoCapitalize="none"
+            />
+            <TextInput
+                style={{marginTop: 10}}
+                onChangeText={text => setPassword(text)}
+                placeholder="Password"
+                secureTextEntry={true}
+                success={success.password}
+                value={password}
+                autoCapitalize="none"
+            />
+            <Button
+                full
+                style={{marginTop: 20}}
+                loading={loading}
+                onPress={() => onSignUp()}>
+              {t('sign_up')}
+            </Button>
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
   );
 }
