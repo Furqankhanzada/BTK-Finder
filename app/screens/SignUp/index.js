6c62a26e632dfc20a3c4a99be0f7ade06e20fa5f
Muhammad Furqan <furqan.khanzada@gmail.com>
2020-09-29 11:52:22 +0500

    Fix indentation for signup and signin merge

--- app/screens/SignUp/index.js
+++ app/screens/SignUp/index.js
@@ -5,7 +5,7 @@ import { Header, SafeAreaView, Icon, Button, TextInput } from '@components';
 import styles from './styles';
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
-import {SIGNUP} from '../../constants';
+import { SIGNUP } from '../../constants';
 
 export default function SignUp({ navigation }) {
   const { colors } = useTheme();
@@ -29,7 +29,6 @@ export default function SignUp({ navigation }) {
    */
   const onSignUp = () => {
     if (name === '' || email === '' || phone === '' || password == '') {
-
       setSuccess({
         ...success,
         name: name != '' ? true : false,
@@ -47,18 +46,19 @@ export default function SignUp({ navigation }) {
 
     console.log('SIGNUP: ', SIGNUP);
 
-    axios.post(SIGNUP, {
-      name,
-      email,
-      phone,
-      password
-    })
-        .then(response => {
-          console.log(response)
-        })
-        .catch(error => {
-          console.log(error.response)
-        });
+    axios
+      .post(SIGNUP, {
+        name,
+        email,
+        phone,
+        password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
 
     console.log('Username: ', name);
     console.log('Email: ', email);
@@ -71,70 +71,70 @@ export default function SignUp({ navigation }) {
     android: 20,
   });
   return (
-      <SafeAreaView style={BaseStyle.safeAreaView} forceInset={{top: 'always'}}>
-        <Header
-            title={t('sign_up')}
-            renderLeft={() => {
-              return (
-                  <Icon
-                      name="arrow-left"
-                      size={20}
-                      color={colors.primary}
-                      enableRTL={true}
-                  />
-              );
-            }}
-            onPressLeft={() => {
-              navigation.goBack();
-            }}
-        />
-        <KeyboardAvoidingView
-            behavior={Platform.OS === 'android' ? 'height' : 'padding'}
-            keyboardVerticalOffset={offsetKeyboard}
-            style={{flex: 1}}>
-          <View style={styles.contain}>
-            <TextInput
-                onChangeText={text => setName(text)}
-                placeholder= "Username"
-                success={success.name}
-                value={name}
+    <SafeAreaView style={BaseStyle.safeAreaView} forceInset={{ top: 'always' }}>
+      <Header
+        title={t('sign_up')}
+        renderLeft={() => {
+          return (
+            <Icon
+              name="arrow-left"
+              size={20}
+              color={colors.primary}
+              enableRTL={true}
             />
-            <TextInput
-                style={{marginTop: 10}}
-                onChangeText={text => setEmail(text)}
-                placeholder= "Email"
-                keyboardType="email-address"
-                success={success.email}
-                value={email}
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={{marginTop: 10}}
-                onChangeText={text => setPhone(text)}
-                placeholder="Phone"
-                keyboardType="numeric"
-                success={success.phone}
-                value={phone}
-                autoCapitalize="none"
-            />
-            <TextInput
-                style={{marginTop: 10}}
-                onChangeText={text => setPassword(text)}
-                placeholder="Password"
-                secureTextEntry={true}
-                success={success.password}
-                value={password}
-                autoCapitalize="none"
-            />
-            <Button
-                full
-                style={{marginTop: 20}}
-                loading={loading}
-                onPress={() => onSignUp()}>
-              {t('sign_up')}
-            </Button>
-          </View>
-        </KeyboardAvoidingView>
-      </SafeAreaView>
+          );
+        }}
+        onPressLeft={() => {
+          navigation.goBack();
+        }}
+      />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'android' ? 'height' : 'padding'}
+        keyboardVerticalOffset={offsetKeyboard}
+        style={{ flex: 1 }}>
+        <View style={styles.contain}>
+          <TextInput
+            onChangeText={(text) => setName(text)}
+            placeholder="Username"
+            success={success.name}
+            value={name}
+          />
+          <TextInput
+            style={{ marginTop: 10 }}
+            onChangeText={(text) => setEmail(text)}
+            placeholder="Email"
+            keyboardType="email-address"
+            success={success.email}
+            value={email}
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={{ marginTop: 10 }}
+            onChangeText={(text) => setPhone(text)}
+            placeholder="Phone"
+            keyboardType="numeric"
+            success={success.phone}
+            value={phone}
+            autoCapitalize="none"
+          />
+          <TextInput
+            style={{ marginTop: 10 }}
+            onChangeText={(text) => setPassword(text)}
+            placeholder="Password"
+            secureTextEntry={true}
+            success={success.password}
+            value={password}
+            autoCapitalize="none"
+          />
+          <Button
+            full
+            style={{ marginTop: 20 }}
+            loading={loading}
+            onPress={() => onSignUp()}>
+            {t('sign_up')}
+          </Button>
+        </View>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
   );
 }
