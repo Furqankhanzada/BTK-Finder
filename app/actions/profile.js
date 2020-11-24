3831484de3357437e2551e5e253cba3c2e9d7a56
MuhammadAliGabol <aligabol2002@gmail.com>
2020-11-24 13:17:31 +0500

    Added Axios Interceptor for token #5

--- app/actions/profile.js
+++ app/actions/profile.js
@@ -1,18 +1,15 @@
-import axios from 'axios';
-import AsyncStorage from '@react-native-community/async-storage';
 import { GET_PROFILE } from '../constants';
 import {
     GET_PROFILE_API_SUCCESS,
     GET_PROFILE_API_ERROR,
 } from '../constants/profile';
 import { handleError } from '../utils';
+import axiosApiInstance from '../interceptor/axios-interceptor';
 
 export const getProfile = (cb) => {
   return async (dispatch)  => {
-    const token = await AsyncStorage.getItem('access_token')
-    axios({
+    axiosApiInstance({
       method: 'GET',
-      headers: { Authorization: `Bearer ${token}` },
       url: GET_PROFILE,
     })
       .then((response) => {
