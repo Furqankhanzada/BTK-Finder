f3666ac023a10f9dac8aa6d6c4c971e55cc6823a
MuhammadAliGabol <aligabol2002@gmail.com>
2020-12-24 21:34:26 +0500

    #44 | renamed account to more and changed functionality

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -7,6 +7,7 @@ import {
   REGISTER_API_SUCCESS,
   LOGIN_API_ERROR,
   LOGIN_API_SUCCESS,
+  SIGNOUT,
   GET_PROFILE_API_SUCCESS,
   GET_PROFILE_API_ERROR,
   EDIT_PROFILE_API,
@@ -25,12 +26,13 @@ const onLogin = (data) => {
 };
 
 export const authentication = (login, callback) => (dispatch) => {
-  //call api and dispatch action case
+  dispatch({ type: SIGNOUT, loading: true });
   setTimeout(() => {
     let data = {
       success: login,
     };
     dispatch(onLogin(data));
+    dispatch({ type: SIGNOUT, loading: false });
     if (typeof callback === 'function') {
       callback({ success: true });
     }
@@ -65,6 +67,7 @@ export const login = (user, cb) => {
     })
       .then(async (response) => {
         dispatch({ type: LOGIN_API_SUCCESS, user: response.data });
+        dispatch(getProfile());
         cb && cb();
         console.log('Token:', response.data);
         try {
@@ -135,16 +138,19 @@ export const editProfile = (payload, cb) => {
 
 export const uploadProfileImage = (payload, form, cb) => (dispatch) => {
   dispatch({ type: PROFILE_UPLOAD_API, loading: true });
-  axiosApiInstance.post(`${UPLOAD}/${payload._id}/profile`, form, {
-    headers: { 'Content-Type': 'multipart/form-data' }
+  axiosApiInstance
+    .post(`${UPLOAD}/${payload._id}/profile`, form, {
+      headers: { 'Content-Type': 'multipart/form-data' },
     })
-      .then((response) => {
-        dispatch({ type: PROFILE_UPLOAD_API, loading: false });
-        dispatch(editProfile({...payload, avatar: response.data.Location}, () => cb()));
-      })
-      .catch((error) => {
-        dispatch({ type: PROFILE_UPLOAD_API, loading: false });
-        cb && cb(error);
-        handleError(error);
-      });
+    .then((response) => {
+      dispatch({ type: PROFILE_UPLOAD_API, loading: false });
+      dispatch(
+        editProfile({ ...payload, avatar: response.data.Location }, () => cb()),
+      );
+    })
+    .catch((error) => {
+      dispatch({ type: PROFILE_UPLOAD_API, loading: false });
+      cb && cb(error);
+      handleError(error);
+    });
 };
--- app/reducers/auth.js
+++ app/reducers/auth.js
@@ -4,12 +4,13 @@ import {
   REGISTER_API_SUCCESS,
   LOGIN_API_ERROR,
   LOGIN_API_SUCCESS,
+  SIGNOUT,
   GET_PROFILE_API_SUCCESS,
   GET_PROFILE_API_ERROR,
   EDIT_PROFILE_API,
   EDIT_PROFILE_API_SUCCESS,
   EDIT_PROFILE_API_ERROR,
-  PROFILE_UPLOAD_API
+  PROFILE_UPLOAD_API,
 } from '../constants/auth';
 
 //initial state.
@@ -22,6 +23,7 @@ const initialState = {
   form: {},
   editProfileLoading: false,
   profileImageLoading: false,
+  signOutLoading: false,
 };
 
 export default function userReducer(state = initialState, action = {}) {
@@ -35,21 +37,27 @@ export default function userReducer(state = initialState, action = {}) {
     case REGISTER_API_ERROR:
       return Object.assign({}, state, action.error);
     case LOGIN_API_SUCCESS:
-      return Object.assign({}, state, action.user, {isLogin: true});
+      return Object.assign({}, state, action.user, { isLogin: true });
     case LOGIN_API_ERROR:
       return Object.assign({}, state, action.error);
+    case SIGNOUT:
+      return Object.assign({}, state, { signOutLoading: action.loading });
     case GET_PROFILE_API_SUCCESS:
       return Object.assign({}, state, { ...action.profile });
     case GET_PROFILE_API_ERROR:
       return Object.assign({}, state, action.error);
     case EDIT_PROFILE_API:
-      return Object.assign({}, state, {editProfileLoading: action.loading} );
+      return Object.assign({}, state, { editProfileLoading: action.loading });
     case EDIT_PROFILE_API_SUCCESS:
-      return Object.assign({}, state, action.user, {editProfileLoading: action.loading} );
+      return Object.assign({}, state, action.user, {
+        editProfileLoading: action.loading,
+      });
     case EDIT_PROFILE_API_ERROR:
-      return Object.assign({}, state, action.error, {editProfileLoading: action.loading});
+      return Object.assign({}, state, action.error, {
+        editProfileLoading: action.loading,
+      });
     case PROFILE_UPLOAD_API:
-      return Object.assign({}, state, {profileImageLoading: action.loading} );
+      return Object.assign({}, state, { profileImageLoading: action.loading });
     default:
       return state;
   }
