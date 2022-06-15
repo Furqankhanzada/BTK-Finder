3201221297757f84db017401926327576e4e9518
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-06-15 15:58:06 +0500

    refactor: dynamic link and converted to ts

---
+++ app/hooks/useDynamicLinks.ts
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import dynamicLinks, {
+  FirebaseDynamicLinksTypes,
+} from '@react-native-firebase/dynamic-links';
+
+import { getLastIndex } from '../utils';
+import * as NavigationService from '../services/NavigationService';
+import DynamicLink = FirebaseDynamicLinksTypes.DynamicLink;
+
+export default function useDynamicLinks() {
+  useEffect(() => {
+    dynamicLinks().getInitialLink().then(onDynamicLinkOpen);
+    const unsubscribe = dynamicLinks().onLink(onDynamicLinkOpen);
+    return () => unsubscribe();
+  });
+
+  const onDynamicLinkOpen = (link: DynamicLink | null) => {
+    if (link && link.url) {
+      const id = getLastIndex(link.url);
+      navigateToBusinessDetail(id);
+    }
+  };
+
+  const navigateToBusinessDetail = (id: string) => {
+    const interval = setInterval(() => {
+      if (NavigationService.isReadyRef.current) {
+        clearInterval(interval);
+        NavigationService.navigate('PlaceDetail', { id });
+      }
+    }, 1000);
+  };
+}
