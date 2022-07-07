034085054d26ea977d5da413de1ed54c01e9a72b
Abdul Basit <abdul_basit313@outlook.com>
2022-07-07 15:55:05 +0500

    chore: added in-app update types

--- app/hooks/useNativeUpdate.ts
+++ app/hooks/useNativeUpdate.ts
@@ -1,7 +1,6 @@
 import { useEffect } from 'react';
 import { Platform } from 'react-native';
 import SpInAppUpdates, {
-  NeedsUpdateResponse,
   IAUUpdateKind,
   StartUpdateOptions,
 } from 'sp-react-native-in-app-updates';
@@ -14,9 +13,8 @@ export const useNativeUpdate = () => {
   useEffect(() => {
     // curVersion is optional if you don't provide it will automatically take from the app using react-native-device-info
     inAppUpdates.checkNeedsUpdate({ curVersion: '0.0.1' }).then((result) => {
-      console.log('result', result);
       if (result.shouldUpdate) {
-        let updateOptions = {};
+        let updateOptions: StartUpdateOptions = {};
         if (Platform.OS === 'android') {
           // android only, on iOS the user will be prompted to go to your app store page
           updateOptions = {
