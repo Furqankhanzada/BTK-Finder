12ade7c8afd43b194eff3aa40f34badf49347ce0
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-08-16 23:11:05 +0500

    refactor: remove un used styling (wip)

--- app/screens/businesses/info/components/Detail.js
+++ app/screens/businesses/info/components/Detail.js
@@ -566,67 +566,8 @@ const styles = StyleSheet.create({
     width: 18,
     height: 18,
   },
-  content: {
-    paddingHorizontal: 20,
-  },
-  tagRate: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 8,
-    paddingVertical: 2,
-    borderRadius: 4,
-  },
-  imgBanner: {
-    width: '100%',
-    height: 250,
-    position: 'absolute',
-  },
-  lineSpace: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-  rateLine: {
-    marginTop: 5,
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  line: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginTop: 20,
-  },
-  contentIcon: {
-    width: 32,
-    height: 32,
-    borderRadius: 16,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  userIcon: {
-    width: 40,
-    height: 40,
-    borderRadius: 20,
-    borderWidth: 1,
-    borderColor: 'white',
-    marginRight: 5,
-  },
   map: {
     ...StyleSheet.absoluteFillObject,
-  },
-  contentInforAction: {
-    marginLeft: 10,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    flex: 1,
-  },
-  lineWorkHours: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 10,
-    justifyContent: 'space-between',
-    borderBottomWidth: 1,
   },
   wrapContent: {
     flexWrap: 'wrap',
