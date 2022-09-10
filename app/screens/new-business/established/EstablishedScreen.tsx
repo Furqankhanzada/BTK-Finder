d602916476b198d7035cc10197ca262d131f7a17
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-09-10 21:18:55 +0500

    refactor: date & time picker modal (wip)

--- app/screens/new-business/established/EstablishedScreen.tsx
+++ app/screens/new-business/established/EstablishedScreen.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import { FlatList, SafeAreaView, View } from 'react-native';
+import React, { Fragment, useState } from 'react';
+import {
+  FlatList,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { Formik } from 'formik';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 import { Header, Text, TextInput, Button } from '@components';
-import { BaseStyle } from '@config';
+import { BaseColor, BaseStyle, useTheme } from '@config';
 
 import { StackScreenProps } from '@react-navigation/stack';
 import { GlobalParamList } from '../../../navigation/models/GlobalParamList';
 import { styles } from '../styles/styles';
+import GlobalStyle from '../../../assets/styling/GlobalStyle';
+import moment from 'moment';
 
 export const EstablishedScreen = ({
   navigation,
@@ -16,10 +26,28 @@ export const EstablishedScreen = ({
   //   navigation.navigate('');
   // };
 
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const toggleDatePicker = () => {
+    setDatePickerVisibility(!isDatePickerVisible);
+  };
+
+  const handleConfirm = (date: any, setFieldValue: any) => {
+    setFieldValue('established', date);
+    toggleDatePicker();
+  };
+
   const navigateToBack = () => {
     navigation.goBack();
   };
 
+  const { colors } = useTheme();
+  const cardColor = colors.card;
+
+  const offsetKeyboard = Platform.select({
+    ios: 0,
+    android: 20,
+  });
   return (
     <SafeAreaView style={BaseStyle.safeAreaView}>
       <Header title="Bhsiness Established" />
@@ -28,29 +56,54 @@ export const EstablishedScreen = ({
         onSubmit={(values) => {
           console.log('Formik Values', values);
         }}>
-        {({ values, handleChange }) => {
+        {({ values, handleChange, errors, setFieldValue }) => {
           return (
-            <>
-              <FlatList
-                style={styles.container}
-                overScrollMode={'never'}
-                scrollEventThrottle={16}
-                data={[1]}
-                renderItem={() => {
-                  return (
-                    <View>
-                      <Text title1 bold>
-                        When you have established your Business in BTK?
+            <Fragment>
+              <ScrollView
+                behavior={Platform.OS === 'android' ? 'height' : 'padding'}
+                keyboardVerticalOffset={offsetKeyboard}
+                style={styles.container}>
+                <View>
+                  <View>
+                    <Text title1 bold>
+                      When you have established your Business in BTK?
+                    </Text>
+                  </View>
+
+                  <View style={GlobalStyle.inputContainer}>
+                    <TouchableOpacity
+                      onPress={() => toggleDatePicker()}
+                      style={[
+                        GlobalStyle.datePickerContainer,
+                        { backgroundColor: cardColor },
+                      ]}>
+                      <Text
+                        style={[
+                          GlobalStyle.datePickerContainerText,
+                          {
+                            color: values.establish
+                              ? colors.text
+                              : BaseColor.grayColor,
+                          },
+                        ]}>
+                        {values.establish
+                          ? moment(values.establish).format('DD/MM/YYYY')
+                          : 'Established Date [YYYY/MM/DD]'}
                       </Text>
-                      <TextInput
-                        style={styles.input}
-                        placeholder="Established Date"
-                        value={values.establish}
-                        onChangeText={handleChange('establish')}
-                      />
-                    </View>
-                  );
-                }}
+                    </TouchableOpacity>
+                    {errors.establish ? (
+                      <Text style={GlobalStyle.errorText}>
+                        {errors.establish}
+                      </Text>
+                    ) : null}
+                  </View>
+                </View>
+              </ScrollView>
+              <DateTimePickerModal
+                isVisible={isDatePickerVisible}
+                mode="date"
+                onConfirm={(date) => handleConfirm(date, setFieldValue)}
+                onCancel={toggleDatePicker}
               />
               {values.establish?.length >= 3 ? (
                 <View style={styles.stickyFooter}>
@@ -58,7 +111,7 @@ export const EstablishedScreen = ({
                   <Button>{'Next'}</Button>
                 </View>
               ) : null}
-            </>
+            </Fragment>
           );
         }}
       </Formik>
