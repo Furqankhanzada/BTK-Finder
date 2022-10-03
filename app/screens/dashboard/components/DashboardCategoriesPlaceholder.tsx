c1cc7bfca6c78a120e1626984798a304ff56aa55
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-10-03 16:28:04 +0500

    refactor: dashboard businesses placeholder

--- app/screens/dashboard/components/DashboardCategoriesPlaceholder.tsx
+++ app/screens/dashboard/components/DashboardCategoriesPlaceholder.tsx
@@ -4,11 +4,28 @@ import { View, StyleSheet } from 'react-native';
 
 export function HorizentalCategoriesPlaceholder() {
   return (
-    <View style={{ paddingLeft: 20, paddingRight: 20 }}>
+    <View style={styles.mainContainer}>
       <Placeholder Animation={Progressive}>
         <View style={styles.container}>
           <PlaceholderLine style={styles.placeholderLine} />
           <PlaceholderLine style={styles.text} />
+        </View>
+      </Placeholder>
+    </View>
+  );
+}
+
+export function HorizentalBusinessPlaceholder() {
+  return (
+    <View>
+      <Placeholder Animation={Progressive}>
+        <View>
+          <PlaceholderLine style={styles.image} />
+          <PlaceholderLine style={styles.name} />
+          <View style={styles.innerContainer}>
+            <PlaceholderLine style={styles.ratingFigure} />
+            <PlaceholderLine style={styles.ratingStars} />
+          </View>
         </View>
       </Placeholder>
     </View>
@@ -25,6 +42,10 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     marginRight: 4,
   },
+  mainContainer: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
   container: {
     flexWrap: 'wrap',
     alignItems: 'center',
@@ -35,5 +56,41 @@ const styles = StyleSheet.create({
     height: 8,
     marginRight: 0,
     marginBottom: 0,
+  },
+  innerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  name: {
+    width: 55,
+    height: 10,
+    marginRight: 0,
+    marginBottom: 0,
+    marginTop: 5,
+  },
+  image: {
+    width: 120,
+    height: 100,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 0,
+    marginRight: 15,
+  },
+  ratingFigure: {
+    width: 25,
+    height: 18,
+    marginRight: 0,
+    marginBottom: 0,
+    marginTop: 0,
+  },
+  ratingStars: {
+    width: 55,
+    height: 5,
+    marginRight: 0,
+    marginBottom: 0,
+    marginTop: 0,
   },
 });
