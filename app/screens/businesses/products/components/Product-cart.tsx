483821f59dfcd6cffc50765cd6a615b91c62c473
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-10-12 00:57:18 +0500

    feat: product cart footer button (wip)

--- app/screens/businesses/products/components/Product-cart.tsx
+++ app/screens/businesses/products/components/Product-cart.tsx
@@ -1,13 +1,65 @@
 import { Button } from '@components';
 import React from 'react';
-import { View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import { BaseColor } from '@config';
 
 const ProductCurt = () => {
   return (
-    <View style={{ paddingHorizontal: 20 }}>
-      <Button>Add to cart</Button>
+    <View style={styles.mainContainer}>
+      <Button>
+        <View style={styles.innerContainer}>
+          <View>
+            <Text style={[styles.cartAmount, { color: BaseColor.whiteColor }]}>
+              $15
+            </Text>
+          </View>
+          <View style={styles.subContainer}>
+            <Text style={[styles.cartText, { color: BaseColor.whiteColor }]}>
+              Add to cart
+            </Text>
+          </View>
+          <View>
+            <Text
+              style={[
+                styles.cartQuantity,
+                { backgroundColor: BaseColor.whiteColor },
+              ]}>
+              3
+            </Text>
+          </View>
+        </View>
+      </Button>
     </View>
   );
 };
 
 export default ProductCurt;
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    paddingHorizontal: 20,
+  },
+  subContainer: {
+    width: 200,
+  },
+  innerContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  cartText: {
+    fontSize: 18,
+    textAlign: 'center',
+    textTransform: 'uppercase',
+  },
+  cartQuantity: {
+    fontSize: 18,
+    textAlign: 'center',
+    width: 30,
+    height: 28,
+    borderRadius: 5,
+  },
+  cartAmount: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
