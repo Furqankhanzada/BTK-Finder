a50184c3680a9d0cf740e16519478621787b0a52
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-10-13 14:38:53 +0500

    chore(checkout): remove inline styling from EcommerceButton

--- app/screens/businesses/components/EcommerceButton.tsx
+++ app/screens/businesses/components/EcommerceButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Pressable } from 'react-native';
+import { Pressable, StyleSheet } from 'react-native';
 import { Tag, Text } from '@components';
 import { BaseColor, useTheme } from '@config';
 
@@ -20,16 +20,12 @@ export default function EcommerceButton({
   return (
     <Pressable
       onPress={onPress}
-      style={{
-        backgroundColor: colors.primary,
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        paddingHorizontal: 10,
-        paddingVertical: 10,
-        borderRadius: 10,
-      }}>
+      style={[
+        styles.container,
+        {
+          backgroundColor: colors.primary,
+        },
+      ]}>
       <Text heavy style={{ color: BaseColor.whiteColor }}>
         {leftText}
       </Text>
@@ -40,3 +36,15 @@ export default function EcommerceButton({
     </Pressable>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderRadius: 10,
+  },
+});
