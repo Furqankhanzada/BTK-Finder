79eeaef7507db21b48ee9d288f6c4cf8cc241989
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-10-14 14:43:23 +0500

    chore(checkout): add variants and price

--- app/screens/businesses/components/Product.tsx
+++ app/screens/businesses/components/Product.tsx
@@ -1,16 +1,54 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View } from 'react-native';
 
-import { Text } from '@components';
-import { CatalogProduct } from '../../../models/graphql';
+import { Text, Tag } from '@components';
+import { CatalogProduct, CatalogProductVariant } from '../../../models/graphql';
 import EcommerceButton from '@screens/businesses/components/EcommerceButton';
+import { useTheme } from '@config';
 
 export default function Product({ item }: { item: CatalogProduct }) {
+  const { colors } = useTheme();
+  const [selectedVariant, setSelectedVariant] =
+    useState<CatalogProductVariant | null>();
+
+  useEffect(() => {
+    if (item.variants && item.variants.length) {
+      setSelectedVariant(item.variants[0]);
+    }
+  }, [item.variants]);
+
+  const onVariantPress = (variant: CatalogProductVariant | null) => {
+    setSelectedVariant(variant);
+  };
+
   return (
-    <View style={{ width: '100%' }}>
-      <Text title3>{item.title}</Text>
+    <View style={styles.container}>
+      <Text title1 heavy>
+        {item.title}
+      </Text>
       <Text caption1>{item.pricing[0]?.displayPrice}</Text>
       <Text>{item.description}</Text>
+      <Text headline>Select Size:</Text>
+      <View style={[styles.variantsContainer]}>
+        {item.variants?.map((variant) => (
+          <Tag
+            onPress={() => onVariantPress(variant)}
+            rate
+            style={[
+              styles.variant,
+              selectedVariant?.optionTitle === variant?.optionTitle
+                ? {
+                    backgroundColor: colors.primaryDark,
+                  }
+                : { backgroundColor: colors.primaryLight },
+            ]}>
+            {variant?.optionTitle}
+          </Tag>
+        ))}
+      </View>
+      <Text heavy headline style={{ marginBottom: 10 }}>
+        {selectedVariant?.pricing.map((price) => price?.displayPrice)}
+      </Text>
       <EcommerceButton
         leftText="Rs.20"
         title="Add to cart"
@@ -20,3 +58,21 @@ export default function Product({ item }: { item: CatalogProduct }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+  },
+  variantsContainer: {
+    flexDirection: 'row',
+    marginTop: 5,
+    marginBottom: 10,
+  },
+  variant: {
+    marginRight: 5,
+  },
+  listEmptyText: {
+    textAlign: 'center',
+    paddingVertical: 20,
+  },
+});
