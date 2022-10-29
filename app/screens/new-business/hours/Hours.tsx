938b591aa9fb6cb335eb359234e52f1d42ada82a
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-10-30 00:11:30 +0500

    refactor: new business open hours screen

---
+++ app/screens/new-business/hours/Hours.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from 'react';
+import { FlatList, SafeAreaView, View } from 'react-native';
+import { Formik } from 'formik';
+
+import { Header, Text, Button, HoursCheckbox } from '@components';
+import { BaseColor, BaseStyle } from '@config';
+import useAddBusinessStore from '../store/Store';
+
+import { StackScreenProps } from '@react-navigation/stack';
+import { GlobalParamList } from '../../../navigation/models/GlobalParamList';
+import { styles } from '../styles/styles';
+
+export const Hours = ({ navigation }: StackScreenProps<GlobalParamList>) => {
+  const openHours = useAddBusinessStore((state: any) => state.openHours);
+  const setOpenHours = useAddBusinessStore((state: any) => state.setOpenHours);
+
+  const [active, setActive] = useState<boolean>(false);
+
+  const updateSelectedDays = (payload: any) => {
+    let array = [...openHours];
+    array.map((el) => {
+      if (el.day === payload.day) {
+        el.isOpen = payload.isOpen;
+        setActive(true);
+        if (payload.to) {
+          el.to = payload.to;
+        }
+        if (payload.from) {
+          el.from = payload.from;
+        }
+      }
+    });
+    setOpenHours(array);
+  };
+
+  const navigateToBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView style={BaseStyle.safeAreaView}>
+      <Header title="Opne Hours" />
+
+      <Formik
+        initialValues={{ hours: openHours }}
+        onSubmit={(values) => {
+          navigation.navigate('Price');
+          setOpenHours(values.hours);
+        }}>
+        {({ values, handleSubmit }) => {
+          return (
+            <>
+              <FlatList
+                style={styles.container}
+                overScrollMode={'never'}
+                scrollEventThrottle={16}
+                data={[1]}
+                renderItem={(item) => {
+                  return (
+                    <View>
+                      <Text style={{ paddingBottom: 20 }} title1 bold>
+                        Set Timings of your Business
+                      </Text>
+
+                      {openHours.map((day: any, index: any) => {
+                        return (
+                          <HoursCheckbox
+                            key={index}
+                            day={day}
+                            getObject={updateSelectedDays}
+                          />
+                        );
+                      })}
+                    </View>
+                  );
+                }}
+              />
+
+              <View style={styles.stickyFooter}>
+                <Button style={styles.footerButtons} onPress={navigateToBack}>
+                  {'Back'}
+                </Button>
+
+                <Button
+                  style={[
+                    styles.footerButtons,
+                    !active ? { backgroundColor: BaseColor.grayColor } : null,
+                  ]}
+                  title="submit"
+                  onPress={handleSubmit}>
+                  {'Next'}
+                </Button>
+              </View>
+            </>
+          );
+        }}
+      </Formik>
+    </SafeAreaView>
+  );
+};
