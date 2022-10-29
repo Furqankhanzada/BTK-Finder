3096c20fb1222d635e0f1b8ea2f33a632998f299
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-10-30 01:05:30 +0500

    refactor: new business website screen

---
+++ app/screens/new-business/website/WebsiteScreen.tsx
@@ -0,0 +1,105 @@
+import React from 'react';
+import { FlatList, SafeAreaView, View } from 'react-native';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+import { Header, Text, TextInput, Button } from '@components';
+import { BaseColor, BaseStyle, useTheme } from '@config';
+import useAddBusinessStore from '../store/Store';
+
+import { StackScreenProps } from '@react-navigation/stack';
+import { GlobalParamList } from '../../../navigation/models/GlobalParamList';
+import { styles } from '../styles/styles';
+
+const webRejex =
+  /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+
+const webSchema = Yup.object({
+  website: Yup.string()
+    .matches(webRejex, 'Please enter website')
+    .required('ex: www.xyz.com'),
+});
+
+export const WebsiteScreen = ({
+  navigation,
+}: StackScreenProps<GlobalParamList>) => {
+  const { colors } = useTheme();
+
+  const website = useAddBusinessStore((state: any) => state.website);
+  const setWebsite = useAddBusinessStore((state: any) => state.setWebsite);
+
+  const navigateToNext = () => {
+    navigation.navigate('Established');
+  };
+
+  const navigateToBack = () => {
+    navigation.goBack();
+  };
+
+  return (
+    <SafeAreaView style={BaseStyle.safeAreaView}>
+      <Header
+        title="Business Website"
+        renderRight={() => {
+          return <Text>Skip</Text>;
+        }}
+        onPressRight={navigateToNext}
+      />
+      <Formik
+        initialValues={{ website: website }}
+        validationSchema={webSchema}
+        onSubmit={(values) => {
+          navigation.navigate('Established');
+          setWebsite(values.website);
+        }}>
+        {({ values, handleChange, handleSubmit }) => {
+          return (
+            <>
+              <FlatList
+                style={styles.container}
+                overScrollMode={'never'}
+                scrollEventThrottle={16}
+                data={[1]}
+                renderItem={() => {
+                  return (
+                    <View>
+                      <Text title1 bold>
+                        What is the official website of your Business ?
+                      </Text>
+                      <TextInput
+                        style={styles.input}
+                        placeholder="Website"
+                        value={values.website}
+                        onChangeText={handleChange('website')}
+                      />
+                    </View>
+                  );
+                }}
+              />
+
+              <View style={styles.stickyFooter}>
+                <Button
+                  style={styles.footerButtons}
+                  onPress={() => navigateToBack()}>
+                  {'Back'}
+                </Button>
+
+                <Button
+                  style={[
+                    styles.footerButtons,
+                    values.website.length < 3
+                      ? { backgroundColor: BaseColor.grayColor }
+                      : null,
+                  ]}
+                  title="submit"
+                  onPress={handleSubmit}>
+                  {'Next'}
+                </Button>
+              </View>
+            </>
+          );
+        }}
+      </Formik>
+    </SafeAreaView>
+  );
+};
