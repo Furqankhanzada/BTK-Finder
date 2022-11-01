cc369463b1210a152b2745abaad9117003c9b6c6
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-11-02 02:21:47 +0500

    chore: update script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -11,6 +11,6 @@ brew install yarn
 
 # Install dependencies you manage with CocoaPods.
 yarn
-pod install
+#pod install
 # the sed command from RN cant find the file... so we have to run it ourselves
-sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_13_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
+#sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_13_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
