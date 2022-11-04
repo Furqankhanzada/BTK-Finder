ab23ef35a411f590525d3229b12d441c26f31d0c
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-11-04 18:20:03 +0500

    refactor: New Add Business Presentable

---
+++ app/screens/new-business/models/AddNewBusinessPresentable.ts
@@ -0,0 +1,11 @@
+export interface NewAddBusinessPresentable {
+    color: string,
+    createdAt: string,
+    icon: string,
+    name?: string,
+    order: number,
+    updatedAt: string,
+    item?: object,
+    keyword?: string,
+    selectedTags: [],
+  }
