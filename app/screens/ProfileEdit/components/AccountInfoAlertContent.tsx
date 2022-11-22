df73bd365c10b5eb620493e15a816dba4931a3f0
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-11-23 00:13:03 +0500

    refactor: user data alert

--- app/screens/ProfileEdit/components/AccountInfoAlertContent.tsx
+++ app/screens/ProfileEdit/components/AccountInfoAlertContent.tsx
@@ -1,53 +1,54 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { useTranslation } from 'react-i18next';
+import { StyleSheet, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 import { Text } from '@components';
 import { BaseColor } from '@config';
 
 import { IconName } from '../../../contexts/alerts-v2/models/Icon';
-
-interface Props {
-  ownedBusinesses?: number;
-  givenReviews?: number;
-  reviewsOnOwnedBusinesses?: number;
-}
+import { DeleteUserAccountResponse } from '@screens/ProfileEdit/queries/mutations';
 
 export default function AccountInfoAlertContent({
-  ownedBusinesses,
-  givenReviews,
-  reviewsOnOwnedBusinesses,
-}: Props) {
-  const { t } = useTranslation();
-
+  ownerOfBusinessesCount,
+  businessesWhereGaveReviewsCount,
+  reviewsOnYourBusinessesCount,
+}: DeleteUserAccountResponse) {
   return (
     <>
       <Icon size={70} name={IconName.Warning} color={BaseColor.redColor} />
-      <Text textAlign="center" title2 style={styles.title}>
+      <Text textAlign="center" header style={styles.title}>
         Account Deletion
       </Text>
-      <Text body1 style={styles.description} numberOfLines={20}>
-        {`You have data associated with your account.\nBy proceeding with the account deletion you will lose all mentioned records.\n${
-          ownedBusinesses === 0
-            ? ''
-            : `\n${t('ownerOfBusinessesCount', {
-                count: ownedBusinesses,
-              })}`
-        }${
-          reviewsOnOwnedBusinesses === 0
-            ? ''
-            : `\n${t('reviewsOnYourBusinessesCount', {
-                count: reviewsOnOwnedBusinesses,
-              })}`
-        }${
-          givenReviews === 0
-            ? ''
-            : `\n${t('businessesWhereGaveReviewsCount', {
-                count: givenReviews,
-              })}`
-        }\n\nNote: Be careful you won't be able to recover it again.`}
-      </Text>
+      <View style={styles.description}>
+        <Text textAlign="center" body1 style={styles.bottomSpacing}>
+          You have data associated with your account. By proceeding with the
+          account deletion you will lose all mentioned records.
+        </Text>
+        {businessesWhereGaveReviewsCount ? (
+          <Text textAlign="center" bold>
+            You gave the {businessesWhereGaveReviewsCount} reviews to other
+            businesses.
+          </Text>
+        ) : null}
+        {ownerOfBusinessesCount ? (
+          <Text textAlign="center" bold>
+            You are the owner of {ownerOfBusinessesCount} businesses.
+          </Text>
+        ) : null}
+        {reviewsOnYourBusinessesCount ? (
+          <Text textAlign="center" bold>
+            You businesses have {reviewsOnYourBusinessesCount} reviews.
+          </Text>
+        ) : null}
+      </View>
+      <View style={styles.description}>
+        <Text textAlign="center" bold accentColor>
+          Note:
+        </Text>
+        <Text textAlign="center" body1>
+          Be careful you won't be able to recover it again.
+        </Text>
+      </View>
     </>
   );
 }
@@ -58,5 +59,8 @@ const styles = StyleSheet.create({
   },
   description: {
     marginTop: 24,
+  },
+  bottomSpacing: {
+    marginBottom: 24,
   },
 });
