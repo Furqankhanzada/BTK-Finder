3384da1d41b3907f31207a7d54eac7e38c1d4829
MuhamamdAliGabol <aligabol2002@gmail.com>
2022-11-24 17:53:32 +0500

    chop: implement upload profile image API with useMutation

--- app/screens/profile/queries/mutations.ts
+++ app/screens/profile/queries/mutations.ts
@@ -1,9 +1,11 @@
+import { useDispatch } from 'react-redux';
 import { useMutation } from '@tanstack/react-query';
 
 import { handleError } from '@utils';
 
 import axiosApiInstance from '../../../interceptor/axios-interceptor';
-import { DELETE_PROFILE, EDIT_PROFILE } from '../../../constants';
+import { DELETE_PROFILE, EDIT_PROFILE, UPLOAD } from '../../../constants';
+import { EDIT_PROFILE_API_SUCCESS } from '../../../constants/auth';
 
 export interface DeleteMutationVar {
   confirm: boolean;
@@ -50,4 +52,43 @@ export const useEditProfile = () => {
         handleError(response.data);
       });
   });
+};
+
+export const useUploadProfileImage = () => {
+  const { mutateAsync: editProfile } = useEditProfile();
+  const dispatch = useDispatch();
+
+  return useMutation(
+    (payload: any) => {
+      return axiosApiInstance
+        .post(`${UPLOAD}/${payload.user._id}/profile`, payload.form, {
+          headers: { 'Content-Type': 'multipart/form-data' },
+        })
+        .then((response) => {
+          const user = payload.user;
+          const result = { user: user, avatar: response.data.Location };
+          return result;
+        })
+        .catch(({ response }) => {
+          handleError(response.data);
+        });
+    },
+    {
+      onSuccess: async (response: any) => {
+        const editUserProfile = await editProfile({
+          ...response.user,
+          avatar: response.avatar,
+        });
+
+        if (editUserProfile !== undefined) {
+          //Update User in Redux
+          //TODO: Will fix once we remove the redux from project.
+          dispatch({
+            type: EDIT_PROFILE_API_SUCCESS,
+            user: { ...response.user, avatar: response.avatar },
+          });
+        }
+      },
+    },
+  );
 };
