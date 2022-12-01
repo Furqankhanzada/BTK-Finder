24784b2cdd55d79334ebf64a95be13a9a489e61c
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-12-01 19:33:57 +0500

    refactor: edit business facilities screen (wip)

--- app/screens/new-business/facilities/FacilitiesScreen.tsx
+++ app/screens/new-business/facilities/FacilitiesScreen.tsx
@@ -3,10 +3,10 @@ import { FlatList, SafeAreaView, TouchableOpacity, View } from 'react-native';
 import remoteConfig from '@react-native-firebase/remote-config';
 
 import { Header, Text, Button, Icon } from '@components';
-import { BaseStyle, useTheme } from '@config';
-import useAddBusinessStore from '../store/Store';
-
+import { BaseColor, BaseStyle, useTheme } from '@config';
 import { StackScreenProps } from '@react-navigation/stack';
+
+import useAddBusinessStore from '../store/Store';
 import { GlobalParamList } from '../../../navigation/models/GlobalParamList';
 import { styles } from '../styles/styles';
 import { NewAddBusinessPresentable } from '../models/AddNewBusinessPresentable';
@@ -22,6 +22,10 @@ export const FacilitiesScreen = ({
   const [facilities, setFacilities] = useState<
     Array<NewAddBusinessPresentable>
   >([]);
+
+  const isEditBusiness = useAddBusinessStore(
+    (state: any) => state.isEditBusiness,
+  );
   const setFacility = useAddBusinessStore((state: any) => state.setFacilities);
   const { colors } = useTheme();
 
@@ -61,11 +65,24 @@ export const FacilitiesScreen = ({
   return (
     <SafeAreaView style={BaseStyle.safeAreaView}>
       <Header
-        title="Select Facilities"
+        title={isEditBusiness ? 'Update Facilites' : 'Select Facilities'}
         renderRight={() => {
-          return <Text>Skip</Text>;
+          return isEditBusiness ? null : <Text>Skip</Text>;
         }}
         onPressRight={navigateToNext}
+        renderLeft={() => {
+          return isEditBusiness ? (
+            <Icon
+              name="arrow-left"
+              size={20}
+              color="#5dade2"
+              enableRTL={true}
+            />
+          ) : null;
+        }}
+        onPressLeft={() => {
+          navigation.navigate('EditBusiness');
+        }}
       />
 
       <View style={styles.contain}>
@@ -112,16 +129,23 @@ export const FacilitiesScreen = ({
           }}
         />
       </View>
-
-      <View style={styles.stickyFooter}>
-        <Button style={styles.footerButtons} onPress={() => navigateToBack()}>
-          {'Back'}
-        </Button>
-        {active === true ? (
-          <Button style={styles.footerButtons} onPress={() => navigateToNext()}>
-            {'Next'}
+      <View
+        style={isEditBusiness ? styles.stickyFooterEdit : styles.stickyFooter}>
+        {isEditBusiness ? null : (
+          <Button style={styles.footerButtons} onPress={navigateToBack}>
+            {'Back'}
           </Button>
-        ) : null}
+        )}
+
+        <Button
+          style={[
+            styles.footerButtons,
+            !active ? { backgroundColor: BaseColor.grayColor } : null,
+          ]}
+          title="submit"
+          onPress={() => navigateToNext()}>
+          {isEditBusiness ? 'Update Facilities' : 'Next'}
+        </Button>
       </View>
     </SafeAreaView>
   );
