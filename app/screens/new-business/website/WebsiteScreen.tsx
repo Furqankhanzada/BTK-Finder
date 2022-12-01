bb4f0caee696222a2288c2e4e1c36b2810505020
shehmir <shahmir.hussain.khanzada@gmail.com>
2022-12-01 20:25:14 +0500

    refactor: edit business Website screen (wip)

--- app/screens/new-business/website/WebsiteScreen.tsx
+++ app/screens/new-business/website/WebsiteScreen.tsx
@@ -3,13 +3,14 @@ import { FlatList, SafeAreaView, View } from 'react-native';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 
-import { Header, Text, TextInput, Button } from '@components';
+import { Header, Text, TextInput, Button, Icon } from '@components';
 import { BaseColor, BaseStyle, useTheme } from '@config';
 import useAddBusinessStore from '../store/Store';
 
 import { StackScreenProps } from '@react-navigation/stack';
 import { GlobalParamList } from '../../../navigation/models/GlobalParamList';
 import { styles } from '../styles/styles';
+import { useBusiness } from '@screens/businesses/queries/queries';
 
 const webRejex =
   /((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
@@ -22,11 +23,16 @@ const webSchema = Yup.object({
 
 export const WebsiteScreen = ({
   navigation,
+  route,
 }: StackScreenProps<GlobalParamList>) => {
   const { colors } = useTheme();
 
+  const { data: businessData } = useBusiness(route?.params?.id);
   const website = useAddBusinessStore((state: any) => state.website);
   const setWebsite = useAddBusinessStore((state: any) => state.setWebsite);
+  const isEditBusiness = useAddBusinessStore(
+    (state: any) => state.isEditBusiness,
+  );
 
   const navigateToNext = () => {
     navigation.navigate('Established');
@@ -39,14 +45,29 @@ export const WebsiteScreen = ({
   return (
     <SafeAreaView style={BaseStyle.safeAreaView}>
       <Header
-        title="Business Website"
+        title={isEditBusiness ? 'Update Website' : 'Business Website '}
         renderRight={() => {
-          return <Text>Skip</Text>;
+          return isEditBusiness ? null : <Text>Skip</Text>;
         }}
         onPressRight={navigateToNext}
+        renderLeft={() => {
+          return isEditBusiness ? (
+            <Icon
+              name="arrow-left"
+              size={20}
+              color="#5dade2"
+              enableRTL={true}
+            />
+          ) : null;
+        }}
+        onPressLeft={() => {
+          navigation.goBack();
+        }}
       />
       <Formik
-        initialValues={{ website: website }}
+        initialValues={{
+          website: isEditBusiness ? businessData?.website : website,
+        }}
         validationSchema={webSchema}
         onSubmit={(values) => {
           navigation.navigate('Established');
@@ -76,24 +97,26 @@ export const WebsiteScreen = ({
                   );
                 }}
               />
-
-              <View style={styles.stickyFooter}>
-                <Button
-                  style={styles.footerButtons}
-                  onPress={() => navigateToBack()}>
-                  {'Back'}
-                </Button>
+              <View
+                style={
+                  isEditBusiness ? styles.stickyFooterEdit : styles.stickyFooter
+                }>
+                {isEditBusiness ? null : (
+                  <Button style={styles.footerButtons} onPress={navigateToBack}>
+                    {'Back'}
+                  </Button>
+                )}
 
                 <Button
                   style={[
                     styles.footerButtons,
-                    values.website.length < 3
+                    values.website.length < 8
                       ? { backgroundColor: BaseColor.grayColor }
                       : null,
                   ]}
                   title="submit"
                   onPress={handleSubmit}>
-                  {'Next'}
+                  {isEditBusiness ? 'Update Website' : 'Next'}
                 </Button>
               </View>
             </>
