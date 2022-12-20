967da3eac6dbcb9044f5e015cdb8bb0ec872e084
Muhammad Furqan <furqan.khanzada@gmail.com>
2022-12-20 14:03:58 +0500

    chore: initialize adMod

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ColorSchemeProvider } from 'react-native-dynamic';
 import Toast from 'react-native-toast-message';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import mobileAds from 'react-native-google-mobile-ads';
 
 import {
   usePushNotifications,
@@ -16,6 +17,12 @@ import { reactQueryClient } from './services/network/client';
 import { AlertsV2Provider } from './contexts/alerts-v2/AlertsV2Context';
 import Navigator from './navigation';
 import { store } from './store';
+
+mobileAds()
+  .initialize()
+  .then(adapterStatuses => {
+    // Initialization complete!
+  });
 
 export default function App() {
   // Firebase Dynamic links handling
