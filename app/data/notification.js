3c3359d9b8a685652c57f474bb7665f117365008
MuhamamdAliGabol <aligabol2002@gmail.com>
2022-12-26 15:05:02 +0500

    refactor: update notification sample data

--- app/data/notification.js
+++ app/data/notification.js
@@ -3,31 +3,42 @@ import { Images } from '@config';
 const NotificationData = [
   {
     id: '0',
-    title: 'Obasey Chidy',
-    description: 'Its time to build a difference ...',
-    image: Images.avata1,
-    date: 'Dec 11, 2019',
+    title: 'Shuttle Service | Grand Opening',
+    description:
+      'Dear Residents: Shuttle service will NOT Available last trip for Ayesha Manzil & Malir Halt at 4pm & 4:15 𝐩𝐦 today 24 December 22.\n\nThese Shuttle buses will be available for the opening ceremony of CENTRAL PARK today to provide the transport facility for Preview Copy Tweet\nFree of Cost\nFor Details click here.',
+    image:
+      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
+    date: 'Dec 24, 2022',
+    isRead: false,
   },
   {
     id: '1',
-    title: 'Steve Garrett',
-    description: 'Its time to build a difference ...',
-    image: Images.avata2,
-    date: 'Dec 11, 2019',
+    title: 'DHA Motors 2',
+    description: 'Visit DHA Motors...',
+    image:
+      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
+    date: 'Dec 20, 2022',
+    isRead: true,
   },
   {
     id: '2',
-    title: 'Luvleen Lawrence',
-    description: 'Its time to build a difference ...',
-    image: Images.avata3,
-    date: 'Dec 11, 2019',
+    title: 'Central Park | Grand Opening',
+    description:
+      'Dear Residents It is to inform you that as per Bahria Town Karachi Community request, instead of 20th December, The opening of the largest Central Park is rescheduled for 24th December 2022\n\nFor Details CLICK Here.',
+    image:
+      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
+    date: 'Dec 18, 2022',
+    isRead: true,
   },
   {
     id: '3',
-    title: 'Tom Hardy',
-    description: 'Its time to build a difference ...',
-    image: Images.avata4,
-    date: 'Dec 11, 2019',
+    title: "Let's Vaccine | Influenza",
+    description:
+      "Cold is not a Flu. Thousand of People are getting admitted with flu. Thousands of deaths every year due to flu.\nLet's vaccine. Get a dose of flu Before it's too late, available at Dr. Navais's Clinic.\n\nFor details CLICK Here.",
+    image:
+      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
+    date: 'Dec 17, 2022',
+    isRead: true,
   },
 ];
 
