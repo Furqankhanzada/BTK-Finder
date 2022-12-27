c0e72517ad656299776e1803d3fc4c45cd889666
MuhamamdAliGabol <aligabol2002@gmail.com>
2022-12-27 15:17:12 +0500

    chore: added mobile ads request configuration

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ColorSchemeProvider } from 'react-native-dynamic';
 import Toast from 'react-native-toast-message';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import mobileAds from 'react-native-google-mobile-ads';
+import mobileAds, { MaxAdContentRating } from 'react-native-google-mobile-ads';
 
 import {
   usePushNotifications,
@@ -19,9 +19,29 @@ import Navigator from './navigation';
 import { store } from './store';
 
 mobileAds()
-  .initialize()
-  .then((adapterStatuses) => {
-    // Initialization complete!
+  .setRequestConfiguration({
+    // Update all future requests suitable for parental guidance
+    maxAdContentRating: MaxAdContentRating.PG,
+
+    // Indicates that you want your content treated as child-directed for purposes of COPPA.
+    tagForChildDirectedTreatment: true,
+
+    // Indicates that you want the ad request to be handled in a
+    // manner suitable for users under the age of consent.
+    tagForUnderAgeOfConsent: true,
+
+    // An array of test device IDs to allow.
+    testDeviceIdentifiers: ['EMULATOR'],
+  })
+  .then(() => {
+    // Request config successfully set!
+    // console.log('@Request config successfully set!');
+    mobileAds()
+      .initialize()
+      .then((adapterStatuses) => {
+        // Initialization complete!
+        // console.log('@Initialization complete!', adapterStatuses);
+      });
   });
 
 export default function App() {
