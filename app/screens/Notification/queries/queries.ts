ec55b73bfa57e2ff06eb7d53dbbe0c90ac790053
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-01-02 18:59:27 +0500

    chore: implement get single notification api

--- app/screens/Notification/queries/queries.ts
+++ app/screens/Notification/queries/queries.ts
@@ -23,3 +23,24 @@ export const useGetNotifications = () =>
       },
     },
   );
+
+export const useNotification = (id: string) =>
+  useQuery(
+    ['notifications', id],
+    (): Promise<any> => {
+      return axiosApiInstance({
+        method: 'GET',
+        url: `${NOTIFICATIONS_API}/${id}`,
+      })
+        .then((response) => response.data)
+        .catch(({ response }) => {
+          handleError(response.data);
+        });
+    },
+    {
+      enabled: !!id,
+      select: (data) => {
+        return data;
+      },
+    },
+  );
