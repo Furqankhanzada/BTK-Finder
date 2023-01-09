e89d94afed3ac9eef660d51fa905f7c34159bc0e
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-01-09 19:38:17 +0500

    chore: invalidate notifications query when making it read

--- app/screens/Notification/queries/mutations.ts
+++ app/screens/Notification/queries/mutations.ts
@@ -1,17 +1,22 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { handleError } from '@utils';
 
 import { NOTIFICATIONS_USER_API } from '../../../constants';
 import axiosApiInstance from '../../../interceptor/axios-interceptor';
 
 export const useNotificationUserSave = () => {
+  const queryClient = useQueryClient();
+
   return useMutation<any, Error, any>((payload) => {
     return axiosApiInstance({
       method: 'POST',
       url: `${NOTIFICATIONS_USER_API}`,
       data: payload,
     })
-      .then((response) => console.log(response.data))
+      .then((response) => {
+        queryClient.invalidateQueries(['notifications']);
+        return response.data;
+      })
       .catch(({ response }) => {
         handleError(response.data);
       });
