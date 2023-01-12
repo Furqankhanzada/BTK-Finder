84f5d0d2ceb4abf5ae7129b1a9621bd1dee26f9f
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-01-12 17:40:07 +0500

    chore: add notification users payload type

--- app/screens/Notification/queries/mutations.ts
+++ app/screens/Notification/queries/mutations.ts
@@ -4,10 +4,16 @@ import { handleError } from '@utils';
 import { NOTIFICATIONS_USER_API } from '../../../constants';
 import axiosApiInstance from '../../../interceptor/axios-interceptor';
 
+interface NotificationUsersPayload {
+  read: boolean;
+  notificationId: string;
+  deviceUniqueId: string;
+}
+
 export const useNotificationUserSave = () => {
   const queryClient = useQueryClient();
 
-  return useMutation<any, Error, any>((payload) => {
+  return useMutation<NotificationUsersPayload, Error, any>((payload) => {
     return axiosApiInstance({
       method: 'POST',
       url: `${NOTIFICATIONS_USER_API}`,
