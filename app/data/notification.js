94dfe89f02161ec522b7ff3d5358223792c76fee
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-01-26 15:46:23 +0500

    revert: notification data file

--- app/data/notification.js
+++ app/data/notification.js
@@ -3,49 +3,31 @@ import { Images } from '@config';
 const NotificationData = [
   {
     id: '0',
-    title: 'Shuttle Service | Grand Opening',
-    description:
-      'Dear Residents: Shuttle service will NOT Available last trip for Ayesha Manzil & Malir Halt at 4pm & 4:15 𝐩𝐦 today 24 December 22.\n\nThese Shuttle buses will be available for the opening ceremony of CENTRAL PARK today to provide the transport facility for Preview Copy Tweet\nFree of Cost\nFor Details click here.',
-    image:
-      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
-    date: 'Dec 24, 2022',
-    link: 'https://www.facebook.com/explore.btk/posts/560497296090920',
-    type: 'facebook',
-    isRead: false,
+    title: 'Obasey Chidy',
+    description: 'Its time to build a difference ...',
+    image: Images.avata1,
+    date: 'Dec 11, 2019',
   },
   {
     id: '1',
-    title: 'Labbaik Drinking Water',
-    description:
-      'Pure & Fresh drinking water at Labbaik Drinking Water, Buy Now!',
-    image:
-      'https://btk-explore-prod.s3.ap-southeast-1.amazonaws.com/users/5fdd03d6ec31e556bb05ae2c/businesses//C3E9C95E-520D-4BB3-B092-BCD46D2B04DE.jpg',
-    date: 'Dec 20, 2022',
-    link: 'explorebtk://businesses/6399c961ae8c1b665391ade0',
-    type: 'business',
-    isRead: false,
+    title: 'Steve Garrett',
+    description: 'Its time to build a difference ...',
+    image: Images.avata2,
+    date: 'Dec 11, 2019',
   },
   {
     id: '2',
-    title: 'Central Park | Grand Opening',
-    description:
-      'Dear Residents It is to inform you that as per Bahria Town Karachi Community request, instead of 20th December, The opening of the largest Central Park is rescheduled for 24th December 2022\n\nFor Details CLICK Here.',
-    image:
-      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
-    date: 'Dec 18, 2022',
-    link: 'https://www.facebook.com/explore.btk/posts/475225528026070',
-    type: 'facebook',
-    isRead: true,
+    title: 'Luvleen Lawrence',
+    description: 'Its time to build a difference ...',
+    image: Images.avata3,
+    date: 'Dec 11, 2019',
   },
   {
     id: '3',
-    title: "Let's Vaccine | Influenza",
-    description:
-      "Cold is not a Flu. Thousand of People are getting admitted with flu. Thousands of deaths every year due to flu.\nLet's vaccine. Get a dose of flu Before it's too late, available at Dr. Navais's Clinic.\n\nFor details CLICK Here.",
-    image:
-      'https://btk-explore-prod.s3-ap-southeast-1.amazonaws.com/assets/logo.png',
-    date: 'Dec 17, 2022',
-    isRead: true,
+    title: 'Tom Hardy',
+    description: 'Its time to build a difference ...',
+    image: Images.avata4,
+    date: 'Dec 11, 2019',
   },
 ];
 
