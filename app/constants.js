e0cd323c8b0c1e48b62096c7013da40decd0a6bf
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-02-01 19:16:43 +0500

    refactor: remove upload notification constant

--- app/constants.js
+++ app/constants.js
@@ -18,5 +18,4 @@ export const NOTIFICATIONS_API = `${Config.API_URL}/notifications`;
 export const NOTIFICATIONS_USER_API = `${Config.API_URL}/notification-users`;
 export const DEVICES_API = `${Config.API_URL}/devices`;
 
-export const UPLOAD = `${Config.API_URL}/files/upload?folder=users`;
-export const UPLOAD_NOTIFICATION = `${Config.API_URL}/files/upload?folder=notifications`;
+export const UPLOAD = `${Config.API_URL}/files/upload?folder=`;
