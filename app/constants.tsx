32603b840c3ae76533f5c7eba18eb1e73f02a1f3
Muhammad Furqan <furqan.khanzada@gmail.com>
2023-02-09 22:00:08 +0500

    chore: indent and change to constants.tsx

--- app/constants.tsx
+++ app/constants.tsx
@@ -8,14 +8,16 @@ export const VERIFY_CODE = `${Config.API_URL}/auth/verifyCode`;
 export const CHANGE_PASSWORD = `${Config.API_URL}/auth/changePassword`;
 
 export const GET_CATEGORIES = `${Config.API_URL}/categories`;
-export const GET_PROFILE = `${Config.API_URL}/auth/profile`;
 
+export const GET_PROFILE = `${Config.API_URL}/auth/profile`;
 export const EDIT_PROFILE = `${Config.API_URL}/users/`;
 export const DELETE_PROFILE = `${Config.API_URL}/users`;
 
 export const BUSINESSES_API = `${Config.API_URL}/businesses`;
+
 export const NOTIFICATIONS_API = `${Config.API_URL}/notifications`;
 export const NOTIFICATIONS_USER_API = `${Config.API_URL}/notification-users`;
+
 export const DEVICES_API = `${Config.API_URL}/devices`;
 
 export const UPLOAD = `${Config.API_URL}/files/upload`;
