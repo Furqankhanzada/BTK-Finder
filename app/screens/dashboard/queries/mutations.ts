329c54d8614d7516aba0463646a39fc23e84e667
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-02-09 12:33:10 +0500

    refactor: remove device presentable file and move it to mutation

--- app/screens/dashboard/queries/mutations.ts
+++ app/screens/dashboard/queries/mutations.ts
@@ -4,11 +4,21 @@ import { handleError } from '@utils';
 
 import axiosApiInstance from '../../../interceptor/axios-interceptor';
 import { DEVICES_API } from '../../../constants';
-import { DevicePresentable } from '../models/DevicePresentable';
+
+type DevicePresentable = {
+  _id: string;
+  createdAt: string;
+  updatedAt: string;
+  userId?: string;
+  deviceUniqueId: string;
+  fcmToken: string;
+  os: string;
+  osVersion: string;
+};
 
 type DevicePayload = Pick<
   DevicePresentable,
-  'deviceUniqueId' | 'fcmToken' | 'os' | 'osVersion'
+  'deviceUniqueId' | 'fcmToken' | 'os' | 'osVersion' | 'userId'
 >;
 
 export const useDeviceRegisteration = () => {
