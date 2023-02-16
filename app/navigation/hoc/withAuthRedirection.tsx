81ec72b1085c81c2be886847af3221b823cab94a
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-02-16 16:58:47 +0500

    chore: update lastroutes type of withAuthRedirection

--- app/navigation/hoc/withAuthRedirection.tsx
+++ app/navigation/hoc/withAuthRedirection.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 import Walkthrough from '@screens/Walkthrough';
 
 export enum LastRoutes {
-  Business = 'Business',
+  NewBusinessStack = 'NewBusinessStack',
   Favourite = 'Favourite',
 }
 
