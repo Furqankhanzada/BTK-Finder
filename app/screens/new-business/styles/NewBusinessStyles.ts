18dc9d46027443d990b95e974bd1f4861522f411
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-02-17 19:27:03 +0500

    chore: update styles

--- app/screens/new-business/styles/NewBusinessStyles.ts
+++ app/screens/new-business/styles/NewBusinessStyles.ts
@@ -4,12 +4,23 @@ export const newBusinessStyles = StyleSheet.create({
   keyboardAvoidView: {
     flex: 1,
   },
+  scrollViewContainerStyle: {
+    flexGrow: 1,
+  },
+  scrollView: {
+    flex: 1,
+  },
   container: {
     paddingHorizontal: 20,
     flex: 1,
     marginTop: 10,
   },
   input: {
+    marginTop: 15,
+  },
+  textArea: {
+    height: 80,
+    padding: 10,
     marginTop: 15,
   },
   stickyFooter: {
