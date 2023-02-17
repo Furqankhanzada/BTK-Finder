c1365df086caf0bea76eda8cedba81cd28f9e50d
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-02-17 17:38:40 +0500

    chore: add new business stylesheet

---
+++ app/screens/new-business/styles/NewBusinessStyles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from 'react-native';
+
+export const newBusinessStyles = StyleSheet.create({
+  keyboardAvoidView: {
+    flex: 1,
+  },
+  container: {
+    paddingHorizontal: 20,
+    flex: 1,
+    marginTop: 10,
+  },
+  input: {
+    marginTop: 15,
+  },
+  stickyFooter: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    marginBottom: 15,
+  },
+  stickyFooterEdit: {
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+    marginBottom: 15,
+  },
+  footerButton: {
+    height: 40,
+  },
+});
