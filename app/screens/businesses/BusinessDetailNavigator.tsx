e887a440201dfee748affccae40dbcc4e9073ed9
Muhammad Ali <58255871+MuhammadAliGabol@users.noreply.github.com>
2023-03-09 00:10:25 +0500

    Enhancement | business store (#275)
    * chore: add get store type function
    
    * refactor: add gym in business type
    
    * chore: add condition to hide cart buttons for gym store types
    
    * chore: add getStoreType in helpers folder
    
    * refactor: get products title function
    
    * chore: update for businesses with products
    
    ---------
    
    Co-authored-by: Muhammad Furqan <furqan.khanzada@gmail.com>

--- app/screens/businesses/BusinessDetailNavigator.tsx
+++ app/screens/businesses/BusinessDetailNavigator.tsx
@@ -12,11 +12,9 @@ import { BaseColor, useTheme, useFont } from '@config';
 import { Icon } from '@components';
 import ProductDetailScreen from '@screens/businesses/product-detail/ProductDetailScreen';
 import { useBusiness } from '@screens/businesses/queries/queries';
-import {
-  BusinessType,
-  ShopStatus,
-} from '@screens/businesses/models/BusinessPresentable';
+import { ShopStatus } from '@screens/businesses/models/BusinessPresentable';
 
+import { getProductsTitle } from './helpers/getProductsTitle';
 import BusinessReviewsScreen from './reviews/BusinessReviewsScreen';
 import BusinessOverviewScreen from './info/BusinessOverviewScreen';
 import BusinessProductsScreen from './products/BusinessProductsScreen';
@@ -111,7 +109,7 @@ export default function BusinessDetailNavigator({
           name="Products"
           component={BusinessProductsScreen}
           options={{
-            title: data.type === BusinessType.restaurant ? 'Menu' : 'Products',
+            title: getProductsTitle(data.type),
             tabBarIcon: ({ color }) => {
               return <Icon solid color={color} name="elementor" size={25} />;
             },
---
+++ app/screens/businesses/helpers/getProductsTitle.ts
@@ -0,0 +1,12 @@
+import { BusinessType } from '../models/BusinessPresentable';
+
+export const getProductsTitle = (type: string) => {
+  switch (type) {
+    case BusinessType.restaurant:
+      return 'Menu';
+    case BusinessType.gym:
+      return 'Packages';
+    default:
+      return 'Products';
+  }
+};
