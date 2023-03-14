4a4cf95d5656f2fcbfc15cb7f4f8b2993dc9ed21
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-03-14 20:05:47 +0500

    chore: Implement login API with useMutation

--- app/screens/auth/apis/mutations.ts
+++ app/screens/auth/apis/mutations.ts
@@ -1,17 +1,28 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useMutation } from '@tanstack/react-query';
 import { handleError } from '@utils';
 import { useAlerts } from '@hooks';
 import { BaseColor } from '@config';
 
 import { IconName } from '../../../contexts/alerts-v2/models/Icon';
-import { SIGNUP } from '../../../constants';
-import axiosApiInstance from '../../../interceptor/axios-interceptor';
+import { SIGNUP, LOGIN } from '../../../constants';
+import axios from 'axios';
+import useAuthStore, { AuthStoreActions } from '../store/Store';
+
+type LoginResponse = {
+  access_token: string;
+};
+
+type LoginPayload = {
+  username: string;
+  password: string;
+};
 
 export const useRegisterAcctount = () => {
   const { showNotification } = useAlerts();
 
   return useMutation<any, Error, any>((payload) => {
-    return axiosApiInstance({
+    return axios({
       method: 'POST',
       url: SIGNUP,
       data: payload,
@@ -25,6 +36,31 @@ export const useRegisterAcctount = () => {
           },
           message: 'You have successfully registered an account, Login Now!',
         });
+        return response.data;
+      })
+      .catch(({ response }) => {
+        handleError(response.data);
+      });
+  });
+};
+
+export const useLogin = () => {
+  const setLogin = useAuthStore((state: AuthStoreActions) => state.setLogin);
+
+  return useMutation<LoginResponse, Error, LoginPayload>((payload) => {
+    return axios({
+      method: 'POST',
+      url: LOGIN,
+      data: payload,
+    })
+      .then(async (response) => {
+        try {
+          await AsyncStorage.setItem(
+            'access_token',
+            response?.data?.access_token,
+          );
+          setLogin(true);
+        } catch (e) {}
         return response.data;
       })
       .catch(({ response }) => {
