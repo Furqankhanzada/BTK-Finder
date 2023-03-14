00d4ce93d5ad7ad6405e34a22ed48fc75ab2b690
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-03-14 20:09:31 +0500

    refactor: remove redux unused actions

--- app/actions/auth.js
+++ app/actions/auth.js
@@ -1,104 +1,23 @@
-import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Toast from 'react-native-toast-message';
-import * as actionTypes from './actionTypes';
 import {
-  LOGIN,
   EDIT_PROFILE,
-  GET_PROFILE,
   UPLOAD,
   RESET_PASSWORD,
   VERIFY_CODE,
   CHANGE_PASSWORD,
 } from '../constants';
 import {
-  LOGIN_API_ERROR,
-  LOGIN_API_SUCCESS,
-  SIGNOUT,
-  GET_PROFILE_API_SUCCESS,
-  GET_PROFILE_API_ERROR,
   EDIT_PROFILE_API,
   EDIT_PROFILE_API_SUCCESS,
   EDIT_PROFILE_API_ERROR,
   PROFILE_UPLOAD_API,
-  LOGGED_IN_SUCCESS,
   RESET_PASSWORD_API,
   VERIFY_CODE_API,
   CHANGE_PASSWORD_API,
 } from '../constants/auth';
 import { handleError } from '../utils';
 import axiosApiInstance from '../interceptor/axios-interceptor';
-
-const onLogin = (data) => {
-  return {
-    type: actionTypes.LOGIN,
-    data,
-  };
-};
-
-export const authentication = (login, callback) => (dispatch) => {
-  dispatch({ type: SIGNOUT, loading: true });
-  setTimeout(() => {
-    let data = {
-      success: login,
-    };
-    dispatch(onLogin(data));
-    dispatch({ type: SIGNOUT, loading: false });
-    AsyncStorage.removeItem('access_token');
-    if (typeof callback === 'function') {
-      callback({ success: true });
-    }
-  }, 500);
-};
-
-export const login = (user, cb) => {
-  return (dispatch) => {
-    axios({
-      method: 'POST',
-      url: LOGIN,
-      data: user,
-    })
-      .then(async (response) => {
-        dispatch({ type: LOGIN_API_SUCCESS, user: response.data });
-        dispatch(getProfile());
-        cb && cb();
-        try {
-          await AsyncStorage.setItem(
-            'access_token',
-            response.data.access_token,
-          );
-        } catch (e) {
-          // saving error
-        }
-      })
-      .catch(({ response }) => {
-        dispatch({ type: LOGIN_API_ERROR, response });
-        cb && cb(response);
-        handleError(response.data);
-      });
-  };
-};
-
-export const getProfile = (cb) => {
-  return async (dispatch) => {
-    axiosApiInstance({
-      method: 'GET',
-      url: GET_PROFILE,
-    })
-      .then((response) => {
-        dispatch({
-          type: GET_PROFILE_API_SUCCESS,
-          profile: response.data,
-        });
-        cb && cb();
-      })
-      .catch((error) => {
-        dispatch({ type: GET_PROFILE_API_ERROR, error });
-        cb && cb(error);
-        handleError(error);
-      });
-  };
-};
 
 export const editProfile = (payload, cb) => {
   return async (dispatch) => {
@@ -141,14 +60,6 @@ export const uploadProfileImage = (payload, form, cb) => (dispatch) => {
       cb && cb(error);
       handleError(error);
     });
-};
-
-export const setIsLogin = () => (dispatch) => {
-  AsyncStorage.getItem('access_token').then((token) => {
-    if (token) {
-      dispatch({ type: LOGGED_IN_SUCCESS });
-    }
-  });
 };
 
 export const resetPassword = (payload, cb) => (dispatch) => {
@@ -211,8 +122,6 @@ export const changePassword = (payload, cb) => (dispatch) => {
         text1: 'Password Changed',
         text2: 'You have successfully changed your password',
       });
-      dispatch({ type: LOGIN_API_SUCCESS });
-      dispatch(getProfile());
       cb() && cb();
     })
     .catch(({ response }) => {
