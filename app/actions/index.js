59122d029bf5f6de115ee0336d4ff33d6d68531c
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-03-21 18:14:44 +0500

    refactor: remove unused reducers

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,4 +1,3 @@
-import * as AuthActions from './auth';
 import * as ApplicationActions from './application';
 
-export { AuthActions, ApplicationActions };
+export { ApplicationActions };
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,12 +1,8 @@
 import { combineReducers } from 'redux';
-import CategoryReducer from './category';
 import BusinessReducer from './business';
 import ApplicationReducer from './application';
-import favorites from './favorites';
 
 export default combineReducers({
-  categories: CategoryReducer,
   application: ApplicationReducer,
   businesses: BusinessReducer,
-  favorites,
 });
