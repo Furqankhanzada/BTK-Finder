53b898243bce16e94abc6922e8c1b6629c4168e4
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-03-22 12:38:42 +0500

    refactor: remove business from reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from 'redux';
-import BusinessReducer from './business';
 import ApplicationReducer from './application';
 
 export default combineReducers({
   application: ApplicationReducer,
-  businesses: BusinessReducer,
 });
