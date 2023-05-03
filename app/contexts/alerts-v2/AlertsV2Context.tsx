9eb28daaeed3421a51e459dbf1440e18879b9aaf
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-05-03 13:32:08 +0500

    revert: remove unnecessary space

--- app/contexts/alerts-v2/AlertsV2Context.tsx
+++ app/contexts/alerts-v2/AlertsV2Context.tsx
@@ -276,7 +276,6 @@ export const AlertsV2Provider: React.FC = (props) => {
 
 function Backdrop(backdropProps: BottomSheetBackdropProps) {
   const { colors } = useTheme();
-
   return (
     <BottomSheetBackdrop
       backgroundColor={colors.text}
