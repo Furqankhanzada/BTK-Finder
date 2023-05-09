50e02b07ebd4704720e3c61ba9589350397ead5d
MuhamamdAliGabol <aligabol2002@gmail.com>
2023-05-09 14:00:44 +0500

    refactor: remove async storage getItem and use custom hook

--- app/hooks/useAppearance.ts
+++ app/hooks/useAppearance.ts
@@ -1,29 +1,35 @@
 import { useEffect } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
+import useLocalStorage from '../services/storage/AsyncStorageService';
+import { LocalStorageKeys } from '../services/storage/models/LocalStorage';
 import useAppStore from '../store/appStore';
 import { Font, ThemeMode } from '../store/models/appStore';
 
 export default function useAppearance() {
   const { setThemeMode, setFont } = useAppStore();
+  const { getItem } = useLocalStorage();
 
   useEffect(() => {
     const getDarkTheme = async () => {
-      const themeMode = (await AsyncStorage.getItem('themeMode')) as ThemeMode;
+      const themeMode = (await getItem(
+        LocalStorageKeys.THEME_MODE,
+      )) as unknown as ThemeMode;
       if (themeMode) {
         setThemeMode(themeMode);
       }
     };
     getDarkTheme();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setThemeMode]);
 
   useEffect(() => {
     const getFont = async () => {
-      const font = (await AsyncStorage.getItem('font')) as Font;
+      const font = (await getItem(LocalStorageKeys.FONT)) as unknown as Font;
       if (font) {
         setFont(font);
       }
     };
     getFont();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [setFont]);
 }
