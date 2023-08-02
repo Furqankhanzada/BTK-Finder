65406b78ffe489e3a344477b19eda55cb8164ef7
MuhammadAliGabol <aligabol2002@gmail.com>
2023-08-03 02:29:11 +0500

    chore: add tabs view in members screen

--- app/screens/businesses/members/list/BusinessMembersScreen.tsx
+++ app/screens/businesses/members/list/BusinessMembersScreen.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import { FlatList, RefreshControl, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View, useWindowDimensions } from 'react-native';
+import { TabView, SceneRendererProps, Route } from 'react-native-tab-view';
 import { StackScreenProps } from '@react-navigation/stack';
 
-import { Header, SafeAreaView, Icon, CardList } from '@components';
-import { BaseStyle, useTheme } from '@config';
-import { useBusiness, useMembers } from '@screens/businesses/queries/queries';
-import { useProfile } from '@screens/settings/profile/queries/queries';
+import { Header, Icon, Text } from '@components';
+import { useTheme } from '@config';
+import { MembershipStatus } from '@screens/settings/profile/models/UserPresentable';
 
-import { MembersStackParamList } from '../../../../navigation/models/BusinessDetailBottomTabParamList';
+import { MembersStackParamList } from 'navigation/models/BusinessDetailBottomTabParamList';
+import ActiveMembers from './ActiveMembers';
+import ArchiveMembers from './ArchiveMembers';
 
 type Props = StackScreenProps<MembersStackParamList, 'Members'>;
 
-export default function BusinessMembersScreen(props: Props) {
-  const { navigation, route } = props;
-  const { data, isRefetching, refetch } = useMembers(route.params.businessId);
-  const { data: business } = useBusiness(route.params.businessId);
-  const { data: user } = useProfile();
+const renderScene = (
+  sceneProps: SceneRendererProps & { route: Route },
+  props: Props,
+) => {
+  const { route } = sceneProps;
+  switch (route.key) {
+    case MembershipStatus.ACTIVE:
+      return <ActiveMembers {...props} />;
+    case MembershipStatus.ARCHIVE:
+      return <ArchiveMembers {...props} />;
+    default:
+      return null;
+  }
+};
 
+export default function BusinessMembersScreen(props: Props) {
+  const layout = useWindowDimensions();
   const { colors } = useTheme();
+  const { navigation, route } = props;
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: MembershipStatus.ACTIVE, title: 'Active' },
+    { key: MembershipStatus.ARCHIVE, title: 'Archived' },
+  ]);
 
   return (
-    <SafeAreaView style={BaseStyle.safeAreaView}>
+    <>
       <Header
         title="Members"
         renderLeft={() => {
@@ -59,60 +79,19 @@ export default function BusinessMembersScreen(props: Props) {
           })
         }
       />
-
-      <View style={styles.container}>
-        <FlatList
-          listKey="members"
-          refreshControl={
-            <RefreshControl
-              title="Pull to refresh"
-              colors={[colors.primary]}
-              tintColor={colors.primary}
-              titleColor={colors.text}
-              refreshing={isRefetching}
-              onRefresh={refetch}
-            />
-          }
-          style={[styles.membersContainer]}
-          data={data}
-          keyExtractor={(item) => item._id}
-          ListEmptyComponent={
-            <View>
-              <Text style={[styles.listEmptyText, { color: colors.text }]}>
-                No members found
-              </Text>
-            </View>
-          }
-          renderItem={({ item }) => (
-            <CardList
-              key={item._id}
-              imageStyle={styles.avatar}
-              image={item.avatar}
-              title={item.name}
-              subtitle={item.email}
-              style={styles.membersList}
-              onPress={() => {}}
-              options={[item.membership.package.name]}
-              editAble={business?.ownerId === user?._id}
-              onPressEdit={() =>
-                navigation.navigate('EditMember', {
-                  businessId: route.params.businessId,
-                  membership: item.membership,
-                })
-              }
-            />
-          )}
-        />
-      </View>
-    </SafeAreaView>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={(renderProps: SceneRendererProps & { route: Route }) =>
+          renderScene(renderProps, props)
+        }
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+      />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-  },
   addButton: {
     borderRadius: 5,
     display: 'flex',
@@ -123,20 +102,5 @@ const styles = StyleSheet.create({
   addButtonText: {
     fontSize: 15,
     marginLeft: 5,
-  },
-  avatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-  },
-  membersContainer: {
-    marginTop: 15,
-  },
-  membersList: {
-    marginTop: 10,
-  },
-  listEmptyText: {
-    textAlign: 'center',
-    paddingVertical: 20,
   },
 });
