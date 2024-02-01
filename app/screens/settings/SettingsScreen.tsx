f083e7cac537fc5c83dd755e0bff81cebb25f7a6
abdulrehmankz1 <khanzadaabdulrehman1@gmail.com>
2024-02-01 18:25:43 +0500

    remove a invoice queryClient

--- app/screens/settings/SettingsScreen.tsx
+++ app/screens/settings/SettingsScreen.tsx
@@ -43,7 +43,6 @@ export default function SettingsScreen(
     queryClient.invalidateQueries(['notifications']);
     queryClient.invalidateQueries(['notifications-count']);
     queryClient.removeQueries(['profile']);
-    queryClient.removeQueries(['invoices']);
   };
 
   const CopyToClipboard = () => {
